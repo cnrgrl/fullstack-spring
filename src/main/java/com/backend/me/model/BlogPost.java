0444552ca9bcb5fdf3a0e25f97f8a5ba552788ae
caner <66187631+cnrgrl@users.noreply.github.com>
2024-02-20 02:00:48 +0100

    blog_post table added

--- src/main/java/com/backend/me/model/BlogPost.java
+++ src/main/java/com/backend/me/model/BlogPost.java
@@ -1,30 +1,22 @@
 package com.backend.me.model;
-
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
-// Example for BlogPost.java
 @Entity
+@Table(name = "blog_post")
 public class BlogPost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
-
     @Column(columnDefinition = "TEXT")
     private String content;
-
     private String imgUrl;
-
     private LocalDateTime date;
-
     private int likes;
-
-    private int comments;
-
+    @Column(columnDefinition = "TEXT")
+    private String comments;
     private boolean isVisible;
-
     // getters and setters
 }
