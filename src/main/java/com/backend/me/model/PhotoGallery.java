cae3bb74a23585070eed9a2d2ee02c88ff95f482
caner <66187631+cnrgrl@users.noreply.github.com>
2024-02-20 02:02:26 +0100

    photo_gallery table added

--- src/main/java/com/backend/me/model/PhotoGallery.java
+++ src/main/java/com/backend/me/model/PhotoGallery.java
@@ -1,4 +1,22 @@
 package com.backend.me.model;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
 public class PhotoGallery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private String imgUrl;
+    private LocalDateTime date;
+    private int likes;
+    @Column(columnDefinition = "TEXT")
+    private String comments;
+    private boolean isVisible;
+
+    // getters and setters
 }
