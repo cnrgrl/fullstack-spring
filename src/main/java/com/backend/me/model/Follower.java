0006707a1930ac70e7af0d7166232cab60ddf5a8
caner <66187631+cnrgrl@users.noreply.github.com>
2024-02-20 01:57:06 +0100

    follower table added

--- src/main/java/com/backend/me/model/Follower.java
+++ src/main/java/com/backend/me/model/Follower.java
@@ -1,4 +1,38 @@
 package com.backend.me.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "follower")
 public class Follower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "lastname", nullable = false, length = 255)
+    private String lastname;
+
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "role", nullable = false, length = 20)
+    private String role;
+
+    @Column(name = "authorization", nullable = false, length = 255)
+    private String authorization;
+
+    @Column(name = "comments", columnDefinition = "INT DEFAULT 0")
+    private int comments;
+
+    @Column(name = "likes", columnDefinition = "INT DEFAULT 0")
+    private int likes;
+
+    @Column(name = "page_visit_count", columnDefinition = "INT DEFAULT 0")
+    private int pageVisitCount;
 }
