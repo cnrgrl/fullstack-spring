c05464b9a5ac16e253ec39102b5d061f29c2833c
caner <66187631+cnrgrl@users.noreply.github.com>
2024-02-20 01:56:38 +0100

    Guest table added

--- src/main/java/com/backend/me/model/Guest.java
+++ src/main/java/com/backend/me/model/Guest.java
@@ -1,4 +1,14 @@
 package com.backend.me.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "guest")
 public class Guest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "page_visit_count", columnDefinition = "INT DEFAULT 0")
+    private int pageVisitCount;
+
 }
