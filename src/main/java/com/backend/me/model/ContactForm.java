c811193a8fa2f84aaea0f760cb471ca55725628e
caner <66187631+cnrgrl@users.noreply.github.com>
2024-02-20 02:01:31 +0100

    contact_form table added

--- src/main/java/com/backend/me/model/ContactForm.java
+++ src/main/java/com/backend/me/model/ContactForm.java
@@ -1,4 +1,21 @@
 package com.backend.me.model;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "contact_form")
 public class ContactForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String message;
+    private String email;
+    private String user_type; //'admin', 'follower', 'guest'
+    private LocalDateTime date;
+    private int comments;
+
+    // getters and setters
 }
